bdcbe7d38f84a48698454c85b9ad436a5c807959
PrainLopez <qym200042@gmail.com>
2024-10-08 17:43:55 +0100

    add typing animation for code

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,38 @@
 <script lang="ts" setup>
 const { width } = useWindowSize();
+
+const codeLines = [
+  "npx nuxi@latest",
+  "bun next build",
+  "pnpm create vite",
+  "go fmt *.go",
+  "git pull -r",
+  "nvm use stable",
+  "tsc --init",
+];
+const code = ref("");
+
+onMounted(async () => {
+  if (width.value >= 1440) {
+    const rand = new Promise<String>((resolve) => {
+      setTimeout(() => {
+        resolve(codeLines[Math.floor(Math.random() * codeLines.length)]);
+      }, 1500);
+    });
+    const codeStr = await rand;
+    for (let ch of codeStr) {
+      await new Promise<void>((resolve) => {
+        setTimeout(resolve, 150);
+      });
+      code.value = code.value + ch;
+    }
+  }
+});
 </script>
 
 <template>
   <section
-    class="relative w-framed max-w-[1120px] mx-auto px-6 py-4 border-x border-slate-300 flex flex-row items-center justify-between"
+    class="relative w-full sm:w-framed max-w-[1120px] mx-auto px-6 py-4 border-x border-slate-300 flex flex-row items-center justify-between"
   >
     <NuxtLink class="font-display text-3xl font-bold text-slate-950" to="/">
       Prain Lopez
@@ -20,7 +48,7 @@ const { width } = useWindowSize();
       v-if="width >= 1440"
       class="absolute -right-2.5 bottom-2 translate-x-full"
     >
-      <code class="font-code text-sm text-slate-700">npx nuxi@latest</code>
+      <code class="font-code text-sm text-slate-700">{{ code }}</code>
       <code
         class="font-code text-sm font-extrabold text-slate-700 animate-textBlink"
         >_
