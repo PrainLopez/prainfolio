30c864e1aa2b9e5244f4ad51aeef7a5690afe1b5
PrainLopez <qym200042@gmail.com>
2024-10-08 03:24:10 +0100

    fix style

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,22 +6,25 @@ const { width } = useWindowSize();
   <section
     class="relative w-framed max-w-[1120px] mx-auto px-6 py-4 border-x border-slate-300 flex flex-row items-center justify-between"
   >
-    <NuxtLink class="font-display text-3xl font-bold text-slate-700" to="/">
+    <NuxtLink class="font-display text-3xl font-bold text-slate-950" to="/">
       Prain Lopez
     </NuxtLink>
     <ul class="flex flex-row align-middle">
-      <li class="mx-3">
+      <li class="mx-3 text-slate-950">
         <NuxtLink href="/">Home</NuxtLink>
       </li>
-      <li class="mx-3">Blog</li>
-      <li class="mx-3">Guestbook</li>
+      <li class="mx-3 text-slate-950">Blog</li>
+      <li class="mx-3 text-slate-950">Guestbook</li>
     </ul>
     <span
       v-if="width >= 1440"
       class="absolute -right-2.5 bottom-2 translate-x-full"
     >
-      <code class="font-code text-sm">npx nuxi@latest</code>
-      <code class="font-code text-sm font-extrabold animate-textBlink">_</code>
+      <code class="font-code text-sm text-slate-700">npx nuxi@latest</code>
+      <code
+        class="font-code text-sm font-extrabold text-slate-700 animate-textBlink"
+        >_
+      </code>
     </span>
   </section>
 </template>
