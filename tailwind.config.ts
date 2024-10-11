b9add3f54a23bfa1298a25edb354096819529404
PrainLopez <qym200042@gmail.com>
2024-10-12 00:46:15 +0100

    Initial commit switching to Next.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,22 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        display: ["Space Grotesk", "Open Sans", "sans-serif"],
+        code: ["Space Mono", "IBM Plex Mono", "Courier Prime"],
+      },
+      width: {
+        framed: "calc(100vw - 100px)",
+      },
+      keyframes: {
+        blink: {
+          "25%, 75%": { opacity: "1" },
+          "50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        textBlink: "1s step-end 1s infinite normal none running blink",
+      },
     },
   },
   plugins: [],
