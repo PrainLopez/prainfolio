4a17938b917032743852bde2ddbb32315ff63e16
PrainLopez <qym200042@gmail.com>
2024-10-13 23:59:07 +0100

    play some svg bg

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import TypingCode from "@/components/typingCode";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-[url('/svg/homeCurve.svg')] bg-[-25%_50%] sm:bg-[25%_50%] bg-cover grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -31,13 +31,6 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
             Deploy now
           </a>
           <a
