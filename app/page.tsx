faffc21973871db79c78896a07ecdde510a7ad79
PrainLopez <qym200042@gmail.com>
2024-10-18 02:18:10 +0100

    finallize header

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
           <h1 className="mb-2 font-display text-5xl font-medium text-slate-950">
             Millennium Developer
           </h1>
-          <span className="mb-2 flex flex-row">
+          <span className="flex flex-row">
             <p className="font-mono text-5xl font-medium text-slate-950">@</p>
             <RollingCode />
           </span>
-          <span className="mt-4 font-sans text-slate-500">
-            Learning is decapsulation.
+          <span className="mt-4 font-sans text-lg text-slate-500">
+            Study is decapsulation.
           </span>
         </section>
       </header>
