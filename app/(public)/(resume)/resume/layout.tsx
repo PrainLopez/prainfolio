caa15256649ecefa6869a86211947bc6cc54ac62
PrainLopez <qym200042@gmail.com>
2024-10-22 16:58:20 +0100

    fix and test style

--- app/(public)/(resume)/resume/layout.tsx
+++ app/(public)/(resume)/resume/layout.tsx
@@ -6,7 +6,7 @@ export default function ResumeLayout({
   return (
     <>
       <main className="sections-yframe flex-grow">
-        <section className="sections-xframe">{children}</section>
+        <section className="sections-xframe py-4">{children}</section>
       </main>
       <footer id="footer" className="sections-yframe h-[70px]">
         <section className="sections-xframe"></section>
